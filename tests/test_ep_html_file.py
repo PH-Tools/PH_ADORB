227ad12600691af4498a393e733b7c0b37a8f871
PH Tools <phtools@bldgtyp.com>
2024-09-20 10:20:31 +0200

    update tests

--- tests/test_ep_html_file.py
+++ tests/test_ep_html_file.py
@@ -7,7 +7,6 @@ from ph_adorb.ep_html_file import (
     MissingTableError,
     NoDataLoadedError,
     load_construction_cost_estimate_data,
-    load_construction_quantities_data,
     load_peak_electric_usage_data,
 )
 
@@ -71,37 +70,6 @@ def test_malformed_peak_electric_usage_htm_file():
         file.write("<html><body><table><tr><td>fake</td></tr></table></body></html>")
 
     file_data = DataFileEPTables(source_file_path=fake_html_file, loader=load_peak_electric_usage_data)
-
-    try:
-        with pytest.raises(MissingTableError):
-            file_data.load_file_data()
-    finally:
-        # -- Cleanup
-        fake_html_file.unlink()
-
-
-@pytest.mark.filterwarnings("ignore::DeprecationWarning")
-def test_load_construction_quantities_data():
-    """Test loading the 'Construction Quantities' table data."""
-    _source_file_path = Path("tests/_test_input/example_annual_tables.htm")
-    file_data = DataFileEPTables(source_file_path=_source_file_path, loader=load_construction_quantities_data)
-
-    with pytest.raises(NoDataLoadedError):
-        file_data.data
-
-    file_data.load_file_data()
-    assert file_data.data is not None
-    assert len(file_data.data) == 51
-
-
-def test_malformed_construction_quantities_htm_file():
-    # -- Create a temporary fake HTML file without the right table
-    fake_html_file = Path("tests/_test_input/fake.htm")
-
-    with open(fake_html_file, "w") as file:
-        file.write("<html><body><table><tr><td>fake</td></tr></table></body></html>")
-
-    file_data = DataFileEPTables(source_file_path=fake_html_file, loader=load_construction_quantities_data)
 
     try:
         with pytest.raises(MissingTableError):
